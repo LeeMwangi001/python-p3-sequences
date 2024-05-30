e27a56c0289dd3f792924deba89bdb61b171f2ae
Lee <lee.mugweru@gmail.com>
2024-05-30 14:23:49 +0300

    Completes lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    elif length == 2:
+        print([0, 1])
+        return
+    
+    fib_seq = [0, 1]
+    for i in range(2, length):
+        next_term = fib_seq[-1] + fib_seq[-2]
+        fib_seq.append(next_term)
+    
+    print(fib_seq)
+
